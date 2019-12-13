a8f38b584c73c48b518a38ec133193fa92a9e611
g609bmsma <g609bmsma@protonmail.com>
2019-12-13 11:03:45 +0000

    EXPORTS TO CSV

--- src/app/view-game-page/view-game-page.component.ts
+++ src/app/view-game-page/view-game-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../interfaces/game';
+import { ExportToCsv } from 'export-to-csv';
 
 const GAME_DATA: Game[] = [];
 
@@ -12,9 +13,27 @@ export class ViewGamePageComponent implements OnInit {
   displayedColumns: string[] = ['name', 'platform', 'genre', 'releaseDate', 'players', 'publisher', 'boxArt'];
   dataSource = GAME_DATA;
 
+  options = {
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalSeparator: '.',
+    showLabels: true,
+    showTitle: true,
+    title: "Games",
+    useTextFile: false,
+    useBom: true,
+    useKeysAsHeaders: true,
+  };
+
+  csvExporter = new ExportToCsv(this.options);
+
   constructor() { }
 
   ngOnInit() {
     GAME_DATA.push(history.state);
+  }
+
+  exportToCSV() {
+    this.csvExporter.generateCsv(GAME_DATA);
   }
 }
